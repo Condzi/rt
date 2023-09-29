388bc0cdf3ade007bd0b02529e4b7a08c8f4f7b4
Konrad Kubacki <Condzi@users.noreply.github.com>
2023-09-29 19:38:29 +0200

    Add Quad shape

--- code/cpu_rt/shapes.cxx
+++ code/cpu_rt/shapes.cxx
@@ -10,4 +10,15 @@ make_sphere(Vec3 center, f32 r) {
           .aabb     = make_aabb_from_extremes(aabb_min, aabb_max),
           .material = NULL};
 }
+
+[[nodiscard]] Quad
+make_quad(Vec3 Q, Vec3 u, Vec3 v) {
+  Vec3 const aabb_min = Q;
+  Vec3 const aabb_max = Q + u + v;
+
+  AABB aabb = make_aabb_from_extremes(aabb_min, aabb_max);
+  aabb      = add_padding_if_too_narrow(aabb);
+
+  return {.Q = Q, .u = u, .v = v, .aabb = aabb, .material = NULL};
+}
 } // namespace rt
--- code/cpu_rt/shapes.hxx
+++ code/cpu_rt/shapes.hxx
@@ -5,6 +5,7 @@ struct Sphere {
   Vec3 center;
   // @Note: can be negative: surface normals will point inward.
   f32  radius;
+
   AABB aabb;
   // Move this somewhere else?
   Material *material;
@@ -13,4 +14,16 @@ struct Sphere {
 // Creates a sphere with empty material.
 [[nodiscard]] Sphere
 make_sphere(Vec3 center, f32 r);
+
+struct Quad {
+  Vec3 Q;    //< lower-left corner
+  Vec3 u, v; // Sides, relative to the corner.
+
+  AABB      aabb;
+  Material *material;
+};
+
+// Create a quad with empty material.
+[[nodiscard]] Quad
+make_quad(Vec3 Q, Vec3 u, Vec3 v);
 } // namespace rt
